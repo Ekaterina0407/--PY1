f3a16c6128b77675f05d219b3580b395e1c384e0
Ekaterina0407 <115543296+Ekaterina0407@users.noreply.github.com>
2022-10-16 18:25:26 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+# not False and True or False and False
+# True and True or False and False
+# True or False
+
+result = True
+
+print(src == result)
